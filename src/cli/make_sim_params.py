bcfcb56c7acdd1041b80011e33cb76dc7b675020
Yutong Bu <yutongbu@hotmail.com>
2022-08-02 21:55:04 +1000

    Asy max iter = 99

--- src/cli/make_sim_params.py
+++ src/cli/make_sim_params.py
@@ -16,8 +16,8 @@ def flist_to_str(lst: List[float]):
     return ','.join([str(x) for x in lst])
 
 def get_sim_params():
-    num_partisans = 1
-    n = 2
+    num_partisans = 5
+    n = 10
     coinslist = None
     # coinslist = make_coin_list(bias=0.6, max_steps=1000)
     # coinslist = [1] * 6000 + [0] * 4000
@@ -42,7 +42,7 @@ def get_sim_params():
         "true_bias": 0.6,
         "tosses_per_iteration": 1,
         "learning_rate": 0.25,
-        "asymptotic_learning_max_iters": 200,
+        "asymptotic_learning_max_iters": 99,
         "DWeps": 1,
         "disruption": num_partisans,
         "log": True,  # need to log to get mean_list
